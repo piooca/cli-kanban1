b441707f2557eb9d4be766c48d956431cc5ff2b6
piooca <probatbicol@gmail.com>
2014-07-16 12:20:59 +0200

    create a place for testing

--- tests.py
+++ tests.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import cli_kanban1
+from clikanban1 import *
+
 
 def create_data():
+    """
+    if this gonna turn into a project, then we will do testings
+    """
     new_task('task1')
     new_task('task2')
     new_task('task3')
